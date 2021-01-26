8dd806c5080edddd33a9b78dfc0d322620e70f98
Janis <janis_k@hotmail.com>
2021-01-26 15:01:22 +0100

    Front- + backend setup
    generated backend Spring project
    generated front end Vue 3 project
    
    Communal work by everyone!

---
+++ backend/src/main/java/com/movieknights/server/MovieknightsApplication.java
@@ -0,0 +1,13 @@
+package com.movieknights.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieknightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieknightsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/movieknights/movieknights/MovieknightsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movieknights.movieknights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieknightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
