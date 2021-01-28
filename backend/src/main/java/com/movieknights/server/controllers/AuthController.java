a9045783877d4351daa57ec56a23acc10164f5fe
Janis <janis_k@hotmail.com>
2021-01-28 16:19:00 +0100

    Got Google sign in working with Sprung and JWT
    Pair programming with Jocke

--- backend/src/main/java/com/movieknights/server/controllers/AuthController.java
+++ backend/src/main/java/com/movieknights/server/controllers/AuthController.java
@@ -1,15 +1,28 @@
 package com.movieknights.server.controllers;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeTokenRequest;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.movieknights.server.entities.User;
+import com.movieknights.server.jwt.JwtResponse;
+import com.movieknights.server.jwt.JwtUtils;
+import com.movieknights.server.repos.UserRepo;
+import com.movieknights.server.services.UserDetailsImpl;
+import com.movieknights.server.services.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/auth")
@@ -17,11 +30,23 @@ public class AuthController {
   @Value("${api.google.client_id}")
   private String CLIENT_ID;
 
-  @Value("${server.port}")
-  private String SERVER_PORT;
-
   @Value("${api.google.client_secret}")
   private String CLIENT_SECRET;
+
+  @Value("${movieknights.app.jwtSecret}")
+  private String PASSWORD_SALT;
+
+  @Autowired
+  AuthenticationManager authenticationManager;
+
+  @Autowired
+  JwtUtils jwtUtils;
+
+  @Autowired
+  UserRepo userRepo;
+
+  @Autowired
+  UserDetailsServiceImpl userService;
 
   @RequestMapping(value = "/storeauthcode", method = RequestMethod.POST)
   public ResponseEntity storeauthcode(@RequestBody String code, @RequestHeader("X-Requested-With") String encoding) {
@@ -29,6 +54,7 @@ public class AuthController {
       // Without the `X-Requested-With` header, this request could be forged. Aborts.
       return new ResponseEntity("Error, wrong headers", HttpStatus.BAD_REQUEST);
     }
+
     GoogleTokenResponse tokenResponse = null;
     try {
       tokenResponse = new GoogleAuthorizationCodeTokenRequest(
@@ -42,20 +68,63 @@ public class AuthController {
           .execute();
     } catch (IOException e) {
       e.printStackTrace();
-      return null;
+      return new ResponseEntity("Error, tokenResponse", HttpStatus.BAD_REQUEST);
     }
 
-    // Store these 3 in your DB
+    // Get user info
+    User user = createOrUpdateUser(tokenResponse);
+    if (user == null) {
+      return new ResponseEntity("Error, no payload from Google", HttpStatus.BAD_REQUEST);
+    }
+
+    // Authenticate and return user details
+    return authenticateUser(user);
+  }
+
+  private ResponseEntity<JwtResponse> authenticateUser(User user) {
+    System.out.println("username: " + user.getUsername() + ", password:" + user.getPassword());
+
+    String password = user.getUsername() + PASSWORD_SALT;
+
+    Authentication authentication = authenticationManager.authenticate(
+        new UsernamePasswordAuthenticationToken(user.getUsername(), password));
+
+    SecurityContextHolder.getContext().setAuthentication(authentication);
+    String jwt = jwtUtils.generateJwtToken(authentication);
+
+    UserDetailsImpl userDetails = (UserDetailsImpl)authentication.getPrincipal();
+
+    return ResponseEntity.ok(new JwtResponse(jwt, userDetails.getUsername(), user.getGoogleAccessToken(), userDetails.getAuthorities()));
+  }
+
+  private User createOrUpdateUser(GoogleTokenResponse tokenResponse) {
     String accessToken = tokenResponse.getAccessToken();
     String refreshToken = tokenResponse.getRefreshToken();
     Long expiresAt = System.currentTimeMillis() + (tokenResponse.getExpiresInSeconds() * 1000);
 
-    // Debug purpose only
-    System.out.println("accessToken: " + accessToken);
-    System.out.println("refreshToken: " + refreshToken);
-    System.out.println("expiresAt: " + expiresAt);
+    GoogleIdToken.Payload payload = null;
+    try {
+      payload = tokenResponse.parseIdToken().getPayload();
+    } catch (IOException e) {
+      e.printStackTrace();
+      return null;
+    }
 
-    // TODO: 2021-01-27 Return the creates user or JWT token 
-    return ResponseEntity.ok("OK");
+    User user;
+    Optional<User> optUser = userRepo.findById(payload.getEmail());
+    user = optUser.orElseGet(User::new);
+
+    String password = payload.getEmail() + PASSWORD_SALT;
+    user.setPassword(userService.getEncoder().encode(password));
+
+    user.setExpiresAt(expiresAt);
+    user.setGoogleAccessToken(accessToken);
+    user.setUsername(payload.getEmail());
+    user.setRefreshToken(refreshToken);
+    user.setName((String)payload.get("name"));
+    user.setPictureUrl((String)payload.get("picture"));
+
+    userRepo.save(user);
+    return user;
   }
 }
