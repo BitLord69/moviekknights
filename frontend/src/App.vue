0f979921d6c5b838cb4fa2cebb8c065ee6a0bc50
nekcoj <55090544+nekcoj@users.noreply.github.com>
2021-01-29 20:05:45 +0100

    soem additional layout changes to make the paginator more responsive

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,4 @@
 <template>
-
   <Suspense>
     <Header />
   </Suspense>
--- frontend/src/views/moviepage.vue
+++ frontend/src/views/moviepage.vue
@@ -1,22 +1,23 @@
 <template>
-<div class="movie-container p-grid nested-grid">
-  <div class="p-lg-2">Filmer i db: {{movieCount}}</div>
-    <div class="p-grid p-jc-center">
-      <Paginator v-model:first="state.first" :rows="18" :totalRecords="movieCount"
-        template="FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink" class="p-lg-7">
-        <template #left>
-          <Button type="button" icon="pi pi-refresh" @click="reset()"/>
-        </template>
-        <template #right>
-          <Button type="button" icon="pi pi-search" />
-        </template>
-      </Paginator>
-
-      <div id="movie-page" class="p-grid p-lg-8 p-jc-center">
-          <Movie class="p-col-12 p-md-6 p-lg-1 p-m-2" :movie="movie" v-for="(movie, index) in state.pagMovies && state.pagMovies.slice(state.first, state.first+18)" :key="index"/>
-      </div>
+<div class="p-grid nested-grid">
+  <div class="p-lg-2 p-col-0"></div>
+  <Paginator v-model:first="state.first" :rows="18" :totalRecords="movieCount"
+    template="FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink" class="p-jc-center p-col-12 p-lg-8">
+    <template #left>
+      <Button type="button" icon="pi pi-refresh" @click="reset()"/>
+    </template>
+    <template #right>
+      <Button type="button" icon="pi pi-search" />
+    </template>
+  </Paginator>
+  <div class="p-lg-2 p-col-0"></div>
+  <div class="movie-container p-grid p-mt-1 p-jc-center">
+    <div class="p-lg-2"></div>
+    <div id="movie-page" class="p-grid p-lg-8 p-jc-evenly">
+        <Movie class="p-col-12 p-md-6 p-lg-1 p-m-2" :movie="movie" v-for="(movie, index) in state.pagMovies && state.pagMovies.slice(state.first, state.first+18)" :key="index"/>
     </div>
-  <div class="p-lg-2"></div>
+    <div class="p-lg-2"></div>
+  </div>
 </div>
 </template>
 
