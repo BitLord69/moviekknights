4aa9b990b319de21c7edb23b95d6b98f38e80e1f
nekcoj <55090544+nekcoj@users.noreply.github.com>
2021-02-02 10:55:08 +0100

    trying to implement calendar in frontend with events from google calendar freebusy
    Co-Authored-By: Janis Karlsson <janis_k@hotmail.com>
    Co-Authored-By: Carljohan-hornestam <55090560+Carljohan-hornestam@users.noreply.github.com>
    Co-Authored-By: Alexander Persson <55090569+miniharald@users.noreply.github.com>

--- frontend/src/views/calendarpage.vue
+++ frontend/src/views/calendarpage.vue
@@ -1,24 +1,98 @@
 <template>
   <div>
     <Button @click="getCalendar" label="Get calendar"></Button>
-    {{result}}
+    <FullCalendar :events="state.events" :options="state.options"/>
+    <pre>
+      {{state.events}}
+      <hr/>
+      {{events}}
+    </pre>
   </div>
 </template>
 
 <script>
 import { extFetch } from "@/modules/extFetch";
-import { ref } from "vue";
+import { ref, reactive } from "vue";
+import FullCalendar from 'primevue/fullcalendar';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
 
 export default {
+  components: { FullCalendar },
   setup() {
     const result = ref(null);
-
+    let state = reactive({
+      events: [],
+      options: {
+                plugins:[dayGridPlugin, timeGridPlugin, interactionPlugin],
+                initialDate: '2021-02-01',
+                headerToolbar: {
+                    left: 'prev,next',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                },
+                editable: false
+            },
+    })
+    const events = [
+				{
+					id: 1,
+					"title": "All Day Event",
+					"start": "2021-02-01"
+				},
+				{
+					"id": 2,
+					"title": "Long Event",
+					"start": "2021-02-07",
+					"end": "2021-02-10"
+				},
+				{
+					"id": 3,
+					"title": "Repeating Event",
+					"start": "2021-02-09T16:00:00"
+				},
+				{
+					"id": 4,
+					"title": "Repeating Event",
+					"start": "2021-02-16T16:00:00"
+				},
+				{
+					"id": 5,
+					"title": "Conference",
+					"start": "2021-02-11",
+					"end": "2021-02-13"
+				},
+				{
+					"id": 6,
+					"title": "Meeting",
+					"start": "2021-02-12T10:30:00",
+					"end": "2021-02-12T12:30:00"
+				}
+			]
     async function getCalendar() {
       result.value = await extFetch("/rest/calendar/freebusy", "GET", undefined, true);
+      console.log("Result: ", result.value);
+      let index = 1;
+      Object.entries(result.value.calendars).forEach( calendar => {
+        calendar[1].busy.forEach( event => {
+          console.log(event)
+        let start = new Date(event.start.value).toISOString()
+        let end = new Date(event.end.value).toISOString()
+          state.events.push({ 
+            "id": index++, 
+            "title": "Busy", 
+            "start": start.substring(0, start.length - 5), 
+            "end": end.substring(0, end.length - 5)
+          })
+        })
+      });
+      console.log("Events: ", state.events);
+      console.log("Primevue events: ", events);
     }
 
-    return { result, getCalendar};
+    return { state, result, getCalendar, events};
   },
 };
 </script>
