a4fca0c2c35bb6af4f9a812fb14bdbcd67e1b572
Carljohan-hornestam <55090560+Carljohan-hornestam@users.noreply.github.com>
2021-02-02 11:45:11 +0100

    Removed redundant code in calendarpage
    Co-Authored-By: Janis Karlsson <janis_k@hotmail.com>
    Co-Authored-By: nekcoj <55090544+nekcoj@users.noreply.github.com>
    Co-Authored-By: Alexander Persson <55090569+miniharald@users.noreply.github.com>

--- frontend/src/views/calendarpage.vue
+++ frontend/src/views/calendarpage.vue
@@ -4,11 +4,6 @@
     <div v-if="state.events.length > 0">
     <FullCalendar :events="state.events" :options="state.options"/>
     </div>
-    <pre>
-      {{state.events}}
-      <hr/>
-      {{events}}
-    </pre>
   </div>
 </template>
 
@@ -38,63 +33,25 @@ export default {
                 editable: true
             },
     })
-    const events = [
-				{
-					id: 1,
-					"title": "All Day Event",
-					"start": "2021-02-01"
-				},
-				{
-					"id": 2,
-					"title": "Long Event",
-					"start": "2021-02-07",
-					"end": "2021-02-10"
-				},
-				{
-					"id": 3,
-					"title": "Repeating Event",
-					"start": "2021-02-09T16:00:00"
-				},
-				{
-					"id": 4,
-					"title": "Repeating Event",
-					"start": "2021-02-16T16:00:00"
-				},
-				{
-					"id": 5,
-					"title": "Conference",
-					"start": "2021-02-11",
-					"end": "2021-02-13"
-				},
-				{
-					"id": 6,
-					"title": "Meeting",
-					"start": "2021-02-12T10:30:00",
-					"end": "2021-02-12T12:30:00"
-				}
-			]
+
     async function getCalendar() {
       result.value = await extFetch("/rest/calendar/freebusy", "GET", undefined, true);
-      console.log("Result: ", result.value);
       let index = 1;
       Object.entries(result.value.calendars).forEach( calendar => {
         calendar[1].busy.forEach( event => {
-          console.log(event)
         let start = new Date(event.start.value).toLocaleString()
         let end = new Date(event.end.value).toLocaleString()
           state.events.push({ 
             "id": index++, 
             "title": "Busy", 
-            "start": start,//.substring(0, start.length - 5), 
-            "end": end//.substring(0, end.length - 5)
+            "start": start,
+            "end": end
           })
         })
       });
-      console.log("Events: ", state.events);
-      console.log("Primevue events: ", events);
     }
 
-    return { state, result, getCalendar, events };
+    return { state, result, getCalendar };
   },
 };
 </script>
