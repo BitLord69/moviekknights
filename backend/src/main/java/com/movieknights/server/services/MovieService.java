6ee548d63b6feb3a6faba21c3980bafa0269dd86
Alexander Persson <55090569+miniharald@users.noreply.github.com>
2021-02-05 10:06:23 +0100

    reversing wrong reverse

--- backend/src/main/java/com/movieknights/server/services/MovieService.java
+++ backend/src/main/java/com/movieknights/server/services/MovieService.java
@@ -142,7 +142,9 @@ public class MovieService {
         String backdropPath = null;
 
         try{
-            releaseDate = new SimpleDateFormat("yyyy-MM-dd").parse((String) movieMap.get("release_date"));
+            SimpleDateFormat simpleReleaseDate = new SimpleDateFormat("yyyy-MM-dd");
+            simpleReleaseDate.setTimeZone(TimeZone.getTimeZone("UTC"));
+            releaseDate = simpleReleaseDate.parse((String) movieMap.get("release_date"));
         } catch (ParseException e) {
             e.printStackTrace();
         }
@@ -180,15 +182,23 @@ public class MovieService {
 
     private Person createPerson(Map<String, Object> p) {
         Map<String, Object> personMap = restTemplate.getForObject("https://api.themoviedb.org/3/person/"
-                + p.get("id") + "?api_key=" + TMDB_KEY, Map.class);
+                + p.get("id") + "?api_key=" + TMDB_KEY + "&language=sv", Map.class);
         if(personMap == null) return null;
 
         Date dob = null;
         Date dod = null;
         String profilePath = null;
         try {
-            if (personMap.get("birthday") != null) dob = new SimpleDateFormat("yyyy-MM-dd").parse((String) personMap.get("birthday"));
-            else if (personMap.get("deathday") != null) dod = new SimpleDateFormat("yyyy-MM-dd").parse((String) personMap.get("deathday"));
+            if (personMap.get("birthday") != null) {
+                SimpleDateFormat simpleDob = new SimpleDateFormat("yyyy-MM-dd");
+                simpleDob.setTimeZone(TimeZone.getTimeZone("UTC"));
+                dob = simpleDob.parse((String) personMap.get("birthday"));
+            }
+            if (personMap.get("deathday") != null) {
+                SimpleDateFormat simpleDod = new SimpleDateFormat("yyyy-MM-dd");
+                simpleDod.setTimeZone(TimeZone.getTimeZone("UTC"));
+                dod = simpleDod.parse((String) personMap.get("deathday"));
+            }
         } catch (ParseException e) {
             e.printStackTrace();
         }
@@ -217,6 +227,6 @@ public class MovieService {
     }
 
     public List<Movie> getMoviesFromDb() {
-        return movieRepo.findAll(Sort.by(Sort.Direction.DESC, "popularity"));
+        return movieRepo.findAll(Sort.by(Sort.Direction.DESC, "movieId"));
     }
 }
