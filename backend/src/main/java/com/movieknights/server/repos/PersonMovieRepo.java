3cb9c3db76a3b273712ed37602d0a40281c0f1eb
Janis <janis_k@hotmail.com>
2021-02-08 23:57:29 +0100

    PersonMovie
    Started on a repo to get a person with all his/her movies...

--- backend/src/main/java/com/movieknights/server/repos/PersonMovieRepo.java
+++ backend/src/main/java/com/movieknights/server/repos/PersonMovieRepo.java
@@ -1,9 +1,9 @@
 package com.movieknights.server.repos;
 
-import com.movieknights.server.entities.Movie;
+import com.movieknights.server.entities.PersonMovie;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MovieRepo extends Neo4jRepository<Movie, Long> {
+public interface PersonMovieRepo extends Neo4jRepository<PersonMovie, Long> {
 }
